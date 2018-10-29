4f43b9a2d14aa8358d36fb68081b043da4dae277
Seth Corker <seth.corker@gmail.com>
2018-10-29 20:26:28 +0000

    Initial commit

---
+++ leap/leap.cpp
@@ -0,0 +1,14 @@
+namespace leap {
+  bool is_leap_year(int year)
+  {
+
+    if (year % 4 == 0) {
+      bool div_by_100 = year % 100 == 0;
+      bool div_by_400 = year % 400 == 0;
+
+      return !div_by_100 || (div_by_100 && div_by_400); 
+    } else {
+      return false;
+    }
+  }
+}
