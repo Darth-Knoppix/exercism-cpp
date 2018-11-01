69cbbdb3a88ee95839659e0f88d84f73c3f695c3
Seth Corker <seth.corker@gmail.com>
2018-11-01 19:10:06 +0000

    Update leap

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,14 +1,18 @@
-namespace leap {
-  bool is_leap_year(int year)
+#include "leap.h"
+
+namespace leap
+{
+bool is_leap_year(int year)
+{
+
+  if (year % 4 == 0)
   {
+    bool div_by_100 = year % 100 == 0;
+    bool div_by_400 = year % 400 == 0;
 
-    if (year % 4 == 0) {
-      bool div_by_100 = year % 100 == 0;
-      bool div_by_400 = year % 400 == 0;
-
-      return !div_by_100 || (div_by_100 && div_by_400); 
-    } else {
-      return false;
-    }
+    return !div_by_100 || (div_by_100 && div_by_400);
   }
+
+  return false;
 }
+} // namespace leap
--- leap/leap.h
+++ leap/leap.h
@@ -1,3 +1,9 @@
-namespace leap {
-  bool is_leap_year(int year);
+#if !defined(LEAP_H)
+#define LEAP_H
+
+namespace leap
+{
+bool is_leap_year(int year);
 }
+
+#endif
